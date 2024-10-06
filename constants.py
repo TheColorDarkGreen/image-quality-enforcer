9738f99794d6f110dae7a02785ce4e323f44379f
1991CRX <160188535+1991CRX@users.noreply.github.com>
2024-10-06 16:35:19 -0400

    move some constants to constants.py

---
+++ constants.py
@@ -0,0 +1,60 @@
+REMOVAL_MESSAGE_SUBJECT = "LOW QUALITY IMAGE"
+
+REMOVAL_COMMENT = """
+# Post Removed: Low Quality Image
+\n
+# Image Resolution Detected: {dpi} DPI
+\n
+---
+**Hi /u/{author}, please follow the instructions below and submit a higher quality image in a new post.**  
+\n
+---
+\n
+1. Export your resume as a [**PDF file**](https://www.adobe.com/uk/acrobat/resources/google-doc-to-pdf.html).  
+\n
+2. Convert it to a **[600 DPI](https://www.adobe.com/uk/creativecloud/photography/discover/dots-per-inch-dpi-resolution.html) PNG file** using https://www.cleverpdf.com/pdf-to-images. https://imgur.com/RxxYFQe     
+\n
+3. On **[DESKTOP (NEW.REDDIT)](https://new.reddit.com/r/{sub}/submit)**, insert the PNG file into a **[TEXT SUBMISSION](https://imgur.com/8iik4YP)**.  
+\n
+---
+\n
+**Please do not:**  
+\n
+* Take a picture of your resume with your phone camera  
+\n
+* Take a screenshot of your resume  
+\n
+* Crop out your margins  
+\n
+* Upload a dark mode version of your resume  
+\n
+---
+"""
+
+REMOVAL_MESSAGE = """
+**please follow the instructions below and submit a higher quality image in a new post.**  
+\n
+# Image Resolution Detected: {dpi} DPI
+\n
+---
+\n
+1. Export your resume as a [**PDF file**](https://www.adobe.com/uk/acrobat/resources/google-doc-to-pdf.html).  
+\n
+2. Convert it to a **[600 DPI](https://www.adobe.com/uk/creativecloud/photography/discover/dots-per-inch-dpi-resolution.html) PNG file** using https://www.cleverpdf.com/pdf-to-images. https://imgur.com/RxxYFQe     
+\n
+3. On **[DESKTOP (NEW.REDDIT)](https://new.reddit.com/r/{sub}/submit)**, insert the PNG file into a **[TEXT SUBMISSION](https://imgur.com/8iik4YP)**.  
+\n
+---
+\n
+**Please do not:**  
+\n
+* Take a picture of your resume with your phone camera  
+\n
+* Take a screenshot of your resume  
+\n
+* Crop out your margins  
+\n
+* Upload a dark mode version of your resume  
+\n
+---
+"""
--- main.py
+++ main.py
@@ -1,72 +1,13 @@
 import logging
 import os
 import sys
+from constants import REMOVAL_MESSAGE_SUBJECT, REMOVAL_COMMENT, REMOVAL_MESSAGE
 
 import praw
 
 SUB_NAME = os.environ["SUB_NAME"]
 MIN_IMAGE_WIDTH_PX = 4000  # 470 DPI
 NUM_POSTS_TO_PROCESS = 10
-REMOVAL_MESSAGE_SUBJECT = "LOW QUALITY IMAGE"
-
-REMOVAL_COMMENT = """
-# Post Removed: Low Quality Image
-\n
-# Image Resolution Detected: {dpi} DPI
-\n
----
-**Hi /u/{author}, please follow the instructions below and submit a higher quality image in a new post.**  
-\n
----
-\n
-1. Export your resume as a [**PDF file**](https://www.adobe.com/uk/acrobat/resources/google-doc-to-pdf.html).  
-\n
-2. Convert it to a **[600 DPI](https://www.adobe.com/uk/creativecloud/photography/discover/dots-per-inch-dpi-resolution.html) PNG file** using https://www.cleverpdf.com/pdf-to-images. https://imgur.com/RxxYFQe     
-\n
-3. On **[DESKTOP (NEW.REDDIT)](https://new.reddit.com/r/{sub}/submit)**, insert the PNG file into a **[TEXT SUBMISSION](https://imgur.com/8iik4YP)**.  
-\n
----
-\n
-**Please do not:**  
-\n
-* Take a picture of your resume with your phone camera  
-\n
-* Take a screenshot of your resume  
-\n
-* Crop out your margins  
-\n
-* Upload a dark mode version of your resume  
-\n
----
-"""
-
-REMOVAL_MESSAGE = """
-**please follow the instructions below and submit a higher quality image in a new post.**  
-\n
-# Image Resolution Detected: {dpi} DPI
-\n
----
-\n
-1. Export your resume as a [**PDF file**](https://www.adobe.com/uk/acrobat/resources/google-doc-to-pdf.html).  
-\n
-2. Convert it to a **[600 DPI](https://www.adobe.com/uk/creativecloud/photography/discover/dots-per-inch-dpi-resolution.html) PNG file** using https://www.cleverpdf.com/pdf-to-images. https://imgur.com/RxxYFQe     
-\n
-3. On **[DESKTOP (NEW.REDDIT)](https://new.reddit.com/r/{sub}/submit)**, insert the PNG file into a **[TEXT SUBMISSION](https://imgur.com/8iik4YP)**.  
-\n
----
-\n
-**Please do not:**  
-\n
-* Take a picture of your resume with your phone camera  
-\n
-* Take a screenshot of your resume  
-\n
-* Crop out your margins  
-\n
-* Upload a dark mode version of your resume  
-\n
----
-"""
 
 
 def setup_logger() -> None:
